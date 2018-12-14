512e11ce4dc6b28e6502f742c0d81952a7ff219a
SoleSensei <dimargfd@gmail.com>
2018-12-14 14:42:47 +0300

    update DomainShifter

--- ParseData/data2domains.py
+++ ParseData/data2domains.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 import os
+from shutil import copy, move
 
 # ------------------------ Variables ------------------------ 
 # datapath = '../datasets/nexet/nexet_2017_1/' # path to dataset directory
 # csvfile = '../datasets/nexet/test.csv' # path to csv file
-datapath = 'dataset/'
-csvfile = 'test.csv'
+datapath = 'nexet_2017_1/'
+csvfile = 'train.csv'
 col_name = 'image_filename' # column name with dataset's filenames
 col_state = 'lighting' # column name with dataset's states 
 domains = {
@@ -38,9 +39,9 @@ class DomainShifter(object):
         
         # Check datasets paths
         if not os.path.exists(data):
-            raise FileNotFoundError(f'No dataset \'{os.path.abspath(data)}\' folder found!')
+            raise FileNotFoundError(f"No dataset '{os.path.abspath(data)}' folder found!")
         if not os.path.exists(file):
-            raise FileNotFoundError(f'No csv file \'{os.path.abspath(file)}\' found!')
+            raise FileNotFoundError(f"No csv file '{os.path.abspath(file)}' found!")
 
         def check_cols(*cols):
             """ Check if columns exist in csv """
@@ -60,20 +61,48 @@ class DomainShifter(object):
 
     def shift_domains(self, mode='copy'):
         """ Creating domain folders and parsing dataset folder by csv """
-        # Creating directories
-        for ndir in self.domains.keys():
-            path = os.path.join(self.dataset, ndir)
-            if not os.path.isdir(path):
-                os.mkdir(path)
-        
+        if mode == 'copy':
+            shift = copy
+        elif mode == 'move':
+            shift = move
+        else:
+            raise Exception(f'Shift Domains: no {mode} found!')
+        print('Shifting domains starts...')
+        print(f'Mode: {shift.__name__}')
         # Caclculate splits
         domain_split = {}
         for state in self.states:
             domain_split[state] = sum(state in v for v in self.domains.values())
+        self
+
+        # Creating directories
+        base = self.dataset
+        for ndir in self.domains.keys():
+            path = os.path.join(base, ndir)
+            if not os.path.isdir(path):
+                os.mkdir(path)
+
+        with open('log.txt', 'a', encoding="utf-8") as log, open('err.txt', 'a', encoding="utf-8") as err:
+            print('------------------', file=err)
+            print('------------------', file=log)
+            for _, row in self.csv.iterrows():
+                name = str(row[col_name])
+                src = os.path.join(base, name)
+                is_shifted = False
+                for item in self.domains.items():
+                    if row[col_state] in item[1]:
+                        dst = os.path.join(base, item[0])
+                        if os.path.exists(src):
+                            shift(src, dst)
+                            print(f'{shift.__name__}: {src} → {dst}')
+                            print(f'{shift.__name__}: {src} → {dst}', file=log)
+                            is_shifted = True
+                        break
+                if not is_shifted:
+                    print(f'{row[col_name]} file not shifted', file=err)
+        print('Shifiting completed!')
 
 
 ds = DomainShifter(datapath, csvfile, domains, col_name, col_state)
-ds.shift_domains(mode=mode)
-print(ds.csv['lighting'][0])
-print(ds.csv.head())
+ds.shift_domains(mode)
 
