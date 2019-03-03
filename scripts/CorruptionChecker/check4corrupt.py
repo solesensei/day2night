ac4c5f407f9b2b63ca8b02765deb337f1796c440
SoleSensei <dimargfd@gmail.com>
2019-03-03 17:38:37 +0300

    update check4corrupt

--- scripts/CorruptionChecker/check4corrupt.py
+++ scripts/CorruptionChecker/check4corrupt.py
@@ -3,30 +3,31 @@ import os, sys
 from shutil import move
 from PIL import Image
 
-datapath = '/mnt/W/prj/data/nexet/nexet_2017_1/' # path to dataset directory
-curr_dir = '/mnt/W/prj/data/nexet/corrupted'
+datapath = '/mnt/w/prj/data/bdd100k/images' # path to dataset directory
+corr_dir = '/mnt/w/prj/data/bdd100k/corrupted_images'
 
-if not os.path.exists(curr_dir):
-    os.mkdir(curr_dir)
+if not os.path.exists(corr_dir):
+    print(f'Creating {os.path.abspath(corr_dir)} directory')
+    os.mkdir(os.path.abspath(corr_dir))
 
 with open('log.txt', 'a') as log:
     print('-------- check corrupted ----------', file=log)
-    curr_count = {}
+    corr_count = {}
     for root, _, files in os.walk(datapath):
         print(f'Checking {root} : ', file=log, end='')
         print(f'Checking {root} : ', end='')
-        curr_count[root] = 0
+        corr_count[root] = 0
         for file in files:
             try:
                 src = os.path.join(root, file)
                 img = Image.open(src) 
                 img.verify()
             except (IOError, SyntaxError) as e:
-                curr_count[root] += 1
-                move(src, curr_dir)
-                print(f'corrupted move: {src} → {curr_dir}', file=log)
-        print(curr_count[root], 'errors', file=log)
-        print(curr_count[root], 'errors')
-    num_errors = sum(v for v in curr_count.values())
+                corr_count[root] += 1
+                move(src, corr_dir)
+                print(f'corrupted move: {src} → {corr_dir}', file=log)
+        print(corr_count[root], 'errors', file=log)
+        print(corr_count[root], 'errors')
+    num_errors = sum(v for v in corr_count.values())
     print(f'Number of corrupted files: {num_errors}', file=log)
 print('Completed!')
