f5fccfee6401828f27d1c82e84b1e7eb29c4c0b3
SoleSensei <dimargfd@gmail.com>
2019-04-02 12:35:40 +0300

    fix scrtipts

--- scripts/DataClassificator/dismatch_data.py
+++ scripts/DataClassificator/dismatch_data.py
@@ -2,38 +2,40 @@ import os, sys
 import pandas as pd
 from shutil import copy, move
 
-csv = './diffs.csv'
-datadir = '../../../data/nexet/'
+csv = './nexet_diffs.csv'
+datadir = '/mnt/w/prj/data/nexet/nexet_2017_1/trainA'
 
 df = pd.read_csv(csv, sep=',', encoding='utf8', skipinitialspace=True)
 
-if not os.path.isdir('dismatch'):
-    os.mkdir('dismatch')
-if not os.path.isdir('dismatch/day'):
-    os.mkdir('dismatch/day')
-if not os.path.isdir('dismatch/night'):
-    os.mkdir('dismatch/night')
-if not os.path.isdir('dismatch/twilight'):
-    os.mkdir('dismatch/twilight')
+if not os.path.isdir(datadir + '/dismatch'):
+    os.mkdir(datadir + '/dismatch')
+if not os.path.isdir(datadir + '/dismatch/day'):
+    os.mkdir(datadir + '/dismatch/day')
+if not os.path.isdir(datadir + '/dismatch/night'):
+    os.mkdir(datadir + '/dismatch/night')
+if not os.path.isdir(datadir + '/dismatch/twilight'):
+    os.mkdir(datadir + '/dismatch/twilight')
 
-num = len(df)
+num_all = len(df)
 already_processed = 0
-for r,_,f in os.walk('dismatch'):
+for r,_,f in os.walk(datadir + '/dismatch'):
     print(r, len(f))
     already_processed += len(f)
-print(already_processed)
-print(num - already_processed, 'processed', end='\r')
-for r,d,f in os.walk(datadir):
-    for i, file in enumerate(f):
-        if file.endswith('.jpg') and df.image_filename.str.contains(file).any():
-            row = df.loc[df.image_filename == file]
-            dst = f'dismatch/{str(row.lighting_was.values[0]).lower()}/{file[:-4]}_{row.lighting_now.values[0]}_{row.pixels_light.values[0]}.jpg'
-            df = df[~df.image_filename.str.contains(file)]
-            print(f'{num - len(df)} processed', end='\r')
-            if not os.path.exists(dst):
-                src = os.path.join(r, file)
-                copy(src, dst)
+print('Already processed:', already_processed)
+with open('log.txt', 'a', encoding="utf-8") as log:
+    print(f'---- start log ----', file=log)
+    for r,d,f in os.walk(datadir):
+        for i, file in enumerate(f):
+            if file.endswith('.jpg') and df.image_filename.str.contains(file).any():
+                row = df.loc[df.image_filename == file]
+                dst = f'{datadir}/dismatch/{str(row.lighting_now.values[0]).lower()}/{file}'
+                df = df[~df.image_filename.str.contains(file)]
+                print(f'{num_all - len(df)} processed', end='\r')
+                if not os.path.exists(dst):
+                    print(f'{file[:-4]}\t{str(row.lighting_was.values[0]).lower()}->{row.lighting_now.values[0]} [{row.pixels_light.values[0]}]', file=log)
+                    src = os.path.join(r, file)
+                    move(src, dst)
             
-print(f'{num - len(df)} moved! {len(df)} last')
+print(f'{num_all - len(df)} moved! {len(df)} last')
 
     
