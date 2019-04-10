6b97373271bb9807e9b68ab10908fd41ceabefea
SoleSensei <dimargfd@gmail.com>
2019-04-10 18:15:29 +0300

    upd run_docker

--- Day2Night/run_docker.sh
+++ Day2Night/run_docker.sh
@@ -2,6 +2,7 @@
 
 NAME=sole
 TAG=pytorch_0.4.1
+RM=""
 PORT=1434
 
 if [ $# -eq 1 ]; then
@@ -14,9 +15,13 @@ elif [ $# -eq 2 ]; then
 	NAME=$1
 	PORT=$2
 elif [ $# -eq 3 ]; then
+	if [ "$3" == "rm" ]; then
+		RM="--rm"
+	else
+		TAG=$3
+	fi
 	NAME=$1
 	PORT=$2
-	TAG=$3
 fi
 
 
@@ -24,5 +29,10 @@ echo "-------------------------------------------------------"
 echo "Creating docker container: $NAME"
 echo "Port binding: $PORT <- $PORT"
 echo "Docker image: solesensei/day2night:$TAG"
+if [ "$RM" == "--rm" ]; then
+	echo "Docker temporary: True"
+else
+	echo "Docker temporary: False"
+fi
 echo "-------------------------------------------------------"
-docker run -it -p $PORT:$PORT --name $NAME --mount type=bind,source=/hpcfs/GRAPHICS2/21d_gon/,target=/mnt/w/prj -w /mnt/w/prj --runtime nvidia -i -t solesensei/day2night:$TAG
+docker run -it -p $PORT:$PORT $RM --name $NAME --mount type=bind,source=/hpcfs/GRAPHICS2/21d_gon/,target=/mnt/w/prj -w /mnt/w/prj --runtime nvidia -i -t solesensei/day2night:$TAG
