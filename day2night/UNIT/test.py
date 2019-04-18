f49beefcb7bc2ad05002317405109cccb51081b1
SoleSensei <dimargfd@gmail.com>
2019-04-18 14:59:23 +0300

    UNIT|MUNIT test update

--- day2night/UNIT/test.py
+++ day2night/UNIT/test.py
@@ -7,6 +7,7 @@ from utils import get_config, pytorch03_to_pytorch04
 from trainer import MUNIT_Trainer, UNIT_Trainer
 import argparse
 from torch.autograd import Variable
+from subprocess import call
 import torchvision.utils as vutils
 import sys
 import torch
@@ -14,25 +15,39 @@ import os
 from torchvision import transforms
 from PIL import Image
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, help="net configuration")
-parser.add_argument('--input', type=str, help="input image path")
-parser.add_argument('--output_folder', type=str, help="output image path")
-parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders")
-parser.add_argument('--style', type=str, default='', help="style image path")
-parser.add_argument('--a2b', type=int, default=1, help="1 for a2b and others for b2a")
-parser.add_argument('--seed', type=int, default=10, help="random seed")
-parser.add_argument('--num_style',type=int, default=10, help="number of styles to sample")
-parser.add_argument('--synchronized', action='store_true', help="whether use synchronized style code or not")
-parser.add_argument('--output_only', action='store_true', help="whether use synchronized style code or not")
-parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
-parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
-opts = parser.parse_args()
+def usage():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, help="net configuration", required=True)
+    parser.add_argument('--input', type=str, help="input image path", required=True)
+    parser.add_argument('--output_folder', type=str, help="output image path", required=True)
+    parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders", required=True)
+    parser.add_argument('--style', type=str, default='', help="style image path")
+    parser.add_argument('--a2b', type=int, default=1, help="1 for a2b and others for b2a")
+    parser.add_argument('--seed', type=int, default=10, help="random seed")
+    parser.add_argument('--num_style',type=int, default=10, help="number of styles to sample")
+    parser.add_argument('--synchronized', action='store_true', help="whether use synchronized style code or not")
+    parser.add_argument('--output_only', action='store_true', help="whether use synchronized style code or not")
+    parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
+    parser.add_argument('--trainer', type=str, default='UNIT', help="MUNIT|UNIT")
+    parser.add_argument('--device', metavar='GPU', nargs='+', help='GPU List', default=["0"])
+    return parser.parse_args()
+
+opts = usage()
+
+# Choose GPU device to run
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
+os.environ["CUDA_VISIBLE_DEVICES"]=",".join(str(x) for x in opts.device)
 
 torch.manual_seed(opts.seed)
 torch.cuda.manual_seed(opts.seed)
 if not os.path.exists(opts.output_folder):
     os.makedirs(opts.output_folder)
+
+# Print System Info 
+print('CUDA Devices')
+call(["nvidia-smi", "--format=csv", "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
+print(f'Available devices {torch.cuda.device_count()}: {", ".join(str(x) for x in opts.device)}')
+print('Active CUDA Device: GPU', torch.cuda.current_device())
 
 print('Load experiment setting')
 config = get_config(opts.config)
