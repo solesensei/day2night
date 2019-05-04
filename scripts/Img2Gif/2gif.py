1f905c62361aa66f18f3679031560b24720f240c
SoleSensei <dimargfd@gmail.com>
2019-05-04 17:37:20 +0300

    add 2gif.py

---
+++ scripts/Img2Gif/2gif.py
@@ -0,0 +1,119 @@
+import os
+from datetime import timedelta
+from tqdm import tqdm
+import imageio
+
+img_dir = '/home/sole/Documents/alderley/FRAMESA/'
+day_vid = '/home/sole/Documents/alderley/day1_orig.avi'
+night_vid = '/home/sole/Documents/alderley/night1_orig.avi'
+
+
+def vid2gif(video_path, gif_path, fps=-1, duration=-1):
+    if not os.path.exists(video_path):
+        print(f'No {video_path} path found')
+        return 1
+    if fps == -1:
+        reader = imageio.get_reader(video_path)
+    else:
+        reader = imageio.get_reader(video_path, fps=fps)
+    vid_format = os.path.splitext(os.path.basename(video_path))[1].upper()
+    vid_fps = reader.get_meta_data()['fps']
+    vid_len = int(reader.get_meta_data()['duration'])
+    vid_flen = str(timedelta(seconds=vid_len))
+    vid_frames = int(vid_len * vid_fps)
+    if duration != -1:
+        f_duration = str(timedelta(seconds=duration))
+        frames = min(int(duration * vid_fps), vid_frames)
+    else:
+        f_duration = 'full'
+
+    print(f'Start converting: {vid_format} -> GIF | {video_path} -> {gif_path}')
+    print(f'FPS: {int(vid_fps)}')
+    print(f'Video Duration: {vid_flen}')
+    print(f'Video Frames: {vid_frames}')
+    print(f'Save Duration: {f_duration}')
+    print(f'Save Frames: {frames}')
+
+    with imageio.get_writer(gif_path, mode='I', fps=fps) as writer:
+        for i,image in enumerate(tqdm(reader, total=frames)):
+            writer.append_data(image)
+            if i+1 == frames:
+                break
+    reader.close()
+    print(f'Complete!')
+
+
+def img2gif(images_dir, gif_path, fps=2, duration=-1):
+    if not os.path.exists(images_dir):
+        print(f'No {images_dir} path found')
+        return 1
+
+    images = []
+    for root, _, imgs in os.walk(images_dir):
+        for image in imgs:
+            if image.endswith('.png') or image.endswith('.jpg'):
+                images.append(os.path.join(root, image))
+    if not images:
+        print(f'No images found in {images_dir}')
+        return 1
+    frames = len(images)
+    if duration != -1:
+        f_duration = str(timedelta(seconds=duration))
+        frames = min(int(duration * fps), len(images))
+    else:
+        f_duration = 'full'
+
+    print(f'Start converting: IMAGES -> GIF | {images_dir} -> {gif_path}')
+    print(f'FPS: {fps}')
+    print(f'Images in folder: {len(images)}')
+    print(f'Save Duration: {f_duration}')
+    print(f'Save Frames: {frames}')
+
+    with imageio.get_writer(gif_path, mode='I', fps=fps) as writer:
+        for i,image in enumerate(tqdm(images, total=frames)):
+            if i == frames:
+                break
+            image = imageio.imread(image)
+            writer.append_data(image)
+    print(f'Complete!')
+
+
+def vid2img(video_path, images_dir, fps=-1, duration=-1):
+    if not os.path.exists(images_dir):
+        print(f'No {images_dir} path found')
+        return 1
+    if fps == -1:
+        reader = imageio.get_reader(video_path)
+    else:
+        reader = imageio.get_reader(video_path, fps=fps)
+    vid_format = os.path.splitext(os.path.basename(video_path))[1].upper()
+    vid_fps = reader.get_meta_data()['fps']
+    vid_len = int(reader.get_meta_data()['duration'])
+    vid_flen = str(timedelta(seconds=vid_len))
+    frames = vid_frames = int(vid_len * vid_fps)
+    if duration != -1:
+        f_duration = str(timedelta(seconds=duration))
+        frames = min(int(duration * vid_fps), vid_frames)
+    else:
+        f_duration = 'full'
+
+    print(f'Start converting: {vid_format} -> IMAGES | {video_path} -> {images_dir}')
+    print(f'FPS: {int(vid_fps)}')
+    print(f'Video Duration: {vid_flen}')
+    print(f'Video Frames: {vid_frames}')
+    print(f'Save Duration: {f_duration}')
+    print(f'Save Frames: {frames}')
+
+    for i, image in enumerate(tqdm(reader, total=frames)):
+        img_name = f'{images_dir}/image{str(i+1).zfill(7)}.png'
+        imageio.imwrite(img_name, image)
+        if i+1 == frames:
+            break
+    reader.close()
+    print(f'Complete!')
+
+
+vid2img(path2vid, './img', fps=4, duration=20)
+img2gif('img', 'img.gif', fps=4)
+# vid2gif(path2vid, path2gif)
+# vid2gif("/home/sole/Documents/alderley/night1_orig.avi", 'night.gif')\
