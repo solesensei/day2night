07219b999d5d1b2524d006b396cd5f5072f9ec09
SoleSensei <dimargfd@gmail.com>
2019-05-06 17:42:14 +0300

    upd config

--- day2night/UNIT/scripts/day2night.sh
+++ day2night/UNIT/scripts/day2night.sh
@@ -34,5 +34,5 @@ echo "-------------------------------------------------------"
 echo "To Train use: bash /mnt/w/prj/UNIT/scripts/train.sh"
 echo "To Test use: bash /mnt/w/prj/UNIT/scripts/test.sh"
 sleep 3
-docker run -it -p $PORT:$PORT $RM --name $NAME --mount type=bind,source=$PWD,target=/mnt/w/prj -w /mnt/w/prj --runtime nvidia -i -t solesensei/day2night:$TAG
+docker run -it -p $PORT:$PORT $RM --name $NAME --mount type=bind,source=$PWD,target=/mnt/w/prj -w /mnt/w/prj/UNIT --runtime nvidia -i -t solesensei/day2night:$TAG
 echo "Script complete"
