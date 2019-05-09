856e291863d4c080ef0fb14dc6b607c0c71994d4
SoleSensei <dimargfd@gmail.com>
2019-05-09 17:19:08 +0300

    add result images

--- day2night/CycleGAN/docker.sh
+++ day2night/CycleGAN/docker.sh
@@ -1,5 +1,4 @@
-docker pull taesungp/pytorch-cyclegan-and-pix2pix
-# nvidia-docker run -it -p 8097:8097 --mount type=bind,source=/mnt/w/prj/data,target=/mnt/w/prj/data cyclegan
-docker run -it -p 8097:8097 --name cycgan --mount type=bind,source=/mnt/w/prj/data,target=/mnt/w/prj/data \
+docker pull solesensei/day2night:cyclegan
+docker run -it -p 8097:8097 --name cycgan --mount type=bind,source=/mnt/w/prj/,target=/mnt/w/prj/ \
                     --mount type=bind,source=/mnt/w/prj/GraduateWork/day2night/CycleGAN/checkpoints,target=/mnt/w/prj/checkpoints \
                     --runtime nvidia -i -t cyclegan:latest
